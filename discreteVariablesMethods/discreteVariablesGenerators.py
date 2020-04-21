5f6715fc9dc59788c45a020a22ca3f5115b09c77
lucia <mlgonzalez9261@gmail.com>
2020-04-21 20:05:11 -0300

    Ex9 and 10 ready

--- discreteVariablesMethods/discreteVariablesGenerators.py
+++ discreteVariablesMethods/discreteVariablesGenerators.py
@@ -1,6 +1,6 @@
 from random import random
 from collections import defaultdict
-from math import exp
+from math import exp , log
 
 class RejectionMethod():
     ''' Implements the rejection method for generating a random variable with probability
@@ -74,6 +74,8 @@ class InverseTransform():
             raise ValueError("Weights must be a dict or a subclass of dict")
         # Sort the weights for improved efficency
         if isinstance(p_i, dict):
+            if sum(p_i.values()) != 1:
+                raise ValueError("The given array does not represent a probability function")            
             self.p_i = sorted(p_i.items(), key=lambda x: x[1], reverse=True)
 
     def generate(self, iterations):
@@ -83,7 +85,6 @@ class InverseTransform():
 
         for _ in range(iterations):
             u = random()
-
             accum = 0
             for key, value in self.p_i:
                 accum += value
@@ -96,14 +97,15 @@ class InverseTransform():
         '''Generates a random int with the probability distribution given'''
         u = random()
         accum = 0
-        value = 0
-        p_i = self.p_i
-        for key,value in p_i:
-            accum += value
+        generatedValue = 0
+        # We initialize the value with the first element of the
+        # sorted probability array
+        for v,prob in self.p_i:
+            accum += prob
             if u <= accum:
-                value = key
-            break
-        return value 
+                generatedValue = v
+                break
+        return generatedValue 
                
     def generateBinomialInt(self,n,p):
         '''Inverse transformation method using p < 0.5 '''
@@ -136,29 +138,57 @@ class InverseTransform():
             F = F + p
         return value
 
-    def generatePoisson(self,L):
-        I = int(L)
-        p = exp(-L)
+    def generatePoisson(self,lamda):
+        lamdaInt = int(lamda)
+        p = exp(-lamda)
         F = p
-        ## Cálculo de F(I)
-        for i in range(1,I+1):
-            p *= L / i
+        ## We calculate F(lamda), that is the larger probability value.
+        for i in range(1,lamdaInt+1):
+            p *= lamda / i
             F += p
         u=random()
-        if u>=F: #recorre I, I+1, I+2, ...
-            while u>F:
-                I += 1
-                p *= L/I
+        value = lamdaInt
+        # We generate a random number, as always
+        if u >= F: 
+            # If u >= F , it means we have to "look" for a number that has greater
+            # probability of occurring
+            while u > F:
+                value += 1
+                p *= lamda/value
                 F += p
-            return I
+            return value
         else:
-            while u < F: #recorre I-1, I-2, ...
+            # If u< F, we look for a number that hast lesser probability of ocurring
+            while u < F:
                 F -= p
-                p *= I / L
-                I -= 1
-            return I + 1    
+                p *= value / lamda
+                value -= 1
+            return value + 1    
+
+    def generateGeometricInt(self,p):
+        u = 1-random()
+        return int(log(u)/log(1-p))+1
+
 
 
 class CompositionMethod():
-    def __init__(self):
-        pass
+    ''' Composition method allows us to generate a random variable X with 
+    probability mass function  
+    P(X=j) = alpha*p_j + (1-alpha)*q_j with j = 0,1,..., and 0 < alpha < 1''' 
+    def __init__(self,X_generator,Y_generator,alpha):
+        if alpha < 0 or alpha > 1:
+            raise ValueError("Alpha must be a number between 0 and 1")
+        self.x_gen = X_generator
+        self.y_gen = Y_generator
+        self.alpha = alpha    
+
+
+    def generateInt(self):
+        u = random()
+        generatedValue = 0
+        if u < self.alpha:
+            generatedValue = self.x_gen()
+        else:
+            generatedValue = self.y_gen()
+        return generatedValue
+
